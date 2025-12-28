29dc56ccef53b103e2f946a52a7e231be8ef02b2
Tre Galloway <tregallowayofficial@gmail.com>
2025-12-27 23:14:15 -0600

    new astro site

--- astro.config.ts
+++ astro.config.ts
@@ -18,8 +18,11 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   site: 'https://astro-erudite.vercel.app',
+
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
@@ -69,16 +72,20 @@ export default defineConfig({
     sitemap(),
     icon(),
   ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   server: {
     port: 1234,
     host: true,
   },
+
   devToolbar: {
     enabled: false,
   },
+
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
@@ -103,4 +110,8 @@ export default defineConfig({
     ],
     remarkPlugins: [remarkMath, remarkEmoji],
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
